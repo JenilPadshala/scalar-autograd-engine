507c03b95e778fd4087519de2d6441dc8af90844
Twilight Coder <44766165+JenilPadshala@users.noreply.github.com>
2026-06-17 20:23:16 +0530

    Update Function.h

--- Include/Function.h
+++ Include/Function.h
@@ -7,6 +7,8 @@ class Function {
 public:
   // All the Value nodes that are inputs to this operation
   std::vector<std::shared_ptr<Value>> inputs;
+  // Weak pointer to the output Value node of this operation (to avoid circular references)
+  std::weak_ptr<Value> output;
 
   // Constructor that accepts the inputs for this operation
   Function(std::vector<std::shared_ptr<Value>> ins) : inputs(ins) {}
