4a907d911d793728ee2b79c1e630d930e8242bbc
Twilight Coder <44766165+JenilPadshala@users.noreply.github.com>
2026-06-17 20:23:19 +0530

    Update Value.cpp

--- Source/Value.cpp
+++ Source/Value.cpp
@@ -32,11 +32,13 @@ public:
 std::shared_ptr<Value> operator+(const std::shared_ptr<Value> &lhs, const std::shared_ptr<Value> &rhs)
 {
   double out_data = lhs->data + rhs->data;
-
   // Create the AddNode context wrapper
   auto ctx = std::make_shared<AddNode>(lhs, rhs);
-  // Return the new Value node pointing to its creator (AddNode)
-  return std::make_shared<Value>(out_data, ctx);
+  // Create the output Value node and set its creator to the AddNode context
+  auto out_val = std::make_shared<Value>(out_data, ctx);
+  ctx->output = out_val;
+  // Return the new Value node
+  return out_val;
 }
 
 // Overloading the '*' operator
@@ -46,7 +48,10 @@ std::shared_ptr<Value> operator*(const std::shared_ptr<Value> &lhs, const std::s
 
   // Create the MulNode context wrapper
   auto ctx = std::make_shared<MulNode>(lhs, rhs);
+  // Create the output Value node and set its creator to the MulNode context
+  auto out_val = std::make_shared<Value>(out_data, ctx);
+  ctx->output = out_val;
   // Return the new Value node pointing to its creator (MulNode)
-  return std::make_shared<Value>(out_data, ctx);
+  return out_val;
 }
 
