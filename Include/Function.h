48bb6bb0b5a12000d5152a8af146f37cdf26fab5
Twilight Coder <44766165+JenilPadshala@users.noreply.github.com>
2026-06-17 20:40:12 +0530

    Refactored AddNode and MulNode
    Refactored AddNode and MulNode from Value.cpp to Function.cpp

--- Include/Function.h
+++ Include/Function.h
@@ -17,3 +17,17 @@ public:
 
   virtual void backward(double output_grad) = 0;
 };
+
+
+// -- Concrete Operation Declarations --
+class AddNode : public Function {
+  public:
+    AddNode(std::shared_ptr<Value> a, std::shared_ptr<Value> b);
+    void backward(double output_grad) override;
+};
+
+class MulNode : public Function {
+  public:
+    MulNode(std::shared_ptr<Value> a, std::shared_ptr<Value> b);
+    void backward(double output_grad) override;
+};
--- Source/Value.cpp
+++ Source/Value.cpp
@@ -1,32 +1,6 @@
 #include "Value.h"
 #include "Function.h"
 
-// Function for Addition Operation
-class AddNode : public Function
-{
-public:
-  AddNode(std::shared_ptr<Value> a, std::shared_ptr<Value> b)
-      : Function({a, b}) {}
-
-  void backward(double output_grad) override
-  {
-    inputs[0]->grad += output_grad * 1.0;
-    inputs[1]->grad += output_grad * 1.0;
-  }
-};
-
-// Function for Multiplication Operation
-class MulNode : public Function
-{
-public:
-  MulNode(std::shared_ptr<Value> a, std::shared_ptr<Value> b)
-      : Function({a, b}) {}
-  void backward(double output_grad) override
-  {
-    inputs[0]->grad += output_grad * inputs[1]->data;
-    inputs[1]->grad += output_grad * inputs[0]->data;
-  }
-};
 
 // Overloading the '+' operator
 std::shared_ptr<Value> operator+(const std::shared_ptr<Value> &lhs, const std::shared_ptr<Value> &rhs)
