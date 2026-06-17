0e144828fc723c2f248cb9df91356a06ca210f8b
Twilight Coder <44766165+JenilPadshala@users.noreply.github.com>
2026-06-17 19:10:32 +0530

    setup Value and Function Classes.

---
+++ Include/Function.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Value.h"
+#include <memory>
+#include <vector>
+
+class Function {
+public:
+  // All the Value nodes that are inputs to this operation
+  std::vector<std::shared_ptr<Value>> inputs;
+
+  // Constructor that accepts the inputs for this operation
+  Function(std::vector<std::shared_ptr<Value>> ins) : inputs(ins) {}
+
+  virtual ~Function() = default;
+
+  virtual void backward(double output_grad) = 0;
+};
---
+++ Include/Value.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <memory>
+
+class Function;
+
+class Value : public std::enable_shared_from_this<Value> {
+public:
+  double data;
+  double grad;
+  std::shared_ptr<Function> creator;
+
+  // Constructor for Leaf Node / user inputs
+  explicit Value(double val) : data(val), grad(0.0), creator(nullptr) {}
+
+  // Constructor for Internal Nodes
+  Value(double val, std::shared_ptr<Function> op_creator)
+      : data(val), grad(0.0), creator(op_creator) {}
+};
+
+// Global operator overload for easy std::cout printing
+inline std::ostream &operator<<(std::ostream &os,
+                                const std::shared_ptr<Value> &val) {
+  if (val) {
+    os << "Value(data=" << val->data << ", grad=" << val->grad << ")";
+  } else {
+    os << "Value(nullptr)";
+  }
+  return os;
+}
